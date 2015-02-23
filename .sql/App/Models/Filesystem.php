583ad5d121152813b4e9c68362b1460bcc5a1bba
Mikel Torres <michel@majesticmedia.ca>
2015-02-23 12:27:31 +0100

    Changed database

--- .sql/App/Models/Filesystem.php
+++ .sql/App/Models/Filesystem.php
@@ -32,6 +32,7 @@ class Filesystem {
     }
 
     public static function createDirectory($path) {
+        echo $path . PHP_EOL;
         $result = mkdir($path,0755,true);
         if ($result) {
             return $path;
