acdf5cae42ac765b639dd5167d95819b73d3d429
ojoven <mikeltorresmail@gmail.com>
2015-03-14 17:58:06 +0100

    Data added

---
+++ .sql/revisions/3/revision.sql
@@ -0,0 +1,9 @@
+/* Create new tables */
+-- Table `table`
+CREATE TABLE `table` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+INSERT INTO `table` VALUES (1),(2),(3);
+
+
