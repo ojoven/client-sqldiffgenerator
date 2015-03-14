e79328307043725c4d0bc91318c4c5c8e566b27c
ojoven <mikeltorresmail@gmail.com>
2015-03-14 18:03:22 +0100

    First revision

--- .sql/revisions/1/revision.sql
+++
@@ -1,4 +0,0 @@
-
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `myprojectsdb` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
-
-USE `myprojectsdb`;
--- .sql/revisions/2/revision.sql
+++
@@ -1,8 +0,0 @@
-/* Create new tables */
--- Table `table`
-CREATE TABLE `table` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
-
