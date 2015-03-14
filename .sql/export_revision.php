7ba82db4503371550c9dfbe71a4533d9efb550e6
ojoven <mikeltorresmail@gmail.com>
2015-03-14 17:47:46 +0100

    DB under version control

--- .sql/export_revision.php
+++ .sql/export_revision.php
@@ -16,7 +16,7 @@ try {
 
     // Measure script time
     $timeElapsed = microtime(true) - $start;
-    echo $timeElapsed . " time elapsed" . PHP_EOL;
+    $app->log(round($timeElapsed,2) . "s elapsed");
 
 } catch (Exception $e) {
 
--- .sql/revisions/2/revision.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM wp_users WHERE 1 AND `ID` = '1' AND `user_login` = 'ojoven' AND `user_pass` = '$P$BkEDjn8OG1M5trQFFsCv4EXPnVkz0f0' AND `user_nicename` = 'ojoven' AND `user_email` = 'michel@majesticmedia.ca' AND `user_url` = '' AND `user_registered` = '2014-03-06 15:32:35' AND `user_activation_key` = '' AND `user_status` = '0' AND `display_name` = 'ojoven';
-
--- New data for table `wp_users`
-INSERT INTO `wp_users`(`ID`,`user_login`,`user_pass`,`user_nicename`,`user_email`,`user_url`,`user_registered`,`user_activation_key`,`user_status`,`display_name`) VALUES ('1','ojoven','lalala','ojoven','michel@majesticmedia.ca','','2014-03-06 15:32:35','','0','ojoven');
-
