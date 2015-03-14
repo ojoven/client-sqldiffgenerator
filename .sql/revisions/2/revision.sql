6d557343bd7cb0f0efd365432fac1faa81208e6e
ojoven <mikeltorresmail@gmail.com>
2015-03-14 17:57:31 +0100

    Table added

---
+++ .sql/revisions/2/revision.sql
@@ -0,0 +1,8 @@
+/* Create new tables */
+-- Table `table`
+CREATE TABLE `table` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+
