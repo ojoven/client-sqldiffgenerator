b2b538ea3b0a72839e548048d954a4f74b45698d
ojoven <mikeltorresmail@gmail.com>
2015-03-14 18:21:36 +0100

    New table

---
+++ .sql/revisions/2/revision.sql
@@ -0,0 +1,8 @@
+/* Create new tables */
+-- Table `second_table`
+CREATE TABLE `second_table` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+
