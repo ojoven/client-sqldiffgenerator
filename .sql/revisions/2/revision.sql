f7b7fe1b6d9bc409d3bf3a2b9a42d03f77449a5f
ojoven <mikeltorresmail@gmail.com>
2015-03-15 16:49:35 +0100

    New table created

--- .sql/revisions/2/revision.sql
+++ .sql/revisions/2/revision.sql
@@ -1,8 +1,7 @@
 /* Create new tables */
 -- Table `first_table`
-CREATE TABLE `first_table` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 
