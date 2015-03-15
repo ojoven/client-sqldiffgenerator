05f19f167f5b317703551406c10ef806d9435d99
ojoven <mikeltorresmail@gmail.com>
2015-03-15 16:47:37 +0100

    New table created

---
+++ .sql/revisions/2/revision.sql
@@ -0,0 +1,8 @@
+/* Create new tables */
+-- Table `first_table`
+CREATE TABLE `first_table` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+
