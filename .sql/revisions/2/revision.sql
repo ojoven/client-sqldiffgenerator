de360bf18625b645948e9733a146dd07486d98d9
ojoven <mikeltorresmail@gmail.com>
2015-03-17 00:13:15 +0100

    nothing

---
+++ .sql/revisions/2/revision.sql
@@ -0,0 +1,8 @@
+/* Create new tables */
+-- Table `second_table`
+CREATE TABLE `second_table` (
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+INSERT INTO `second_table` VALUES (0),(111),(222),(333);
+
+
